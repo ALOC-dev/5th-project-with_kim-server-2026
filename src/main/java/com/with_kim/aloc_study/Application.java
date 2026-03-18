c0b721e274dbc66119a32ded7fec66fe7793a28b
KimJungMook <wjdanr0869@naver.com>
2026-03-18 16:46:56 +0900

    initial setting

---
+++ src/main/java/com/with_kim/aloc_study/Application.java
@@ -0,0 +1,13 @@
+package com.with_kim.aloc_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
