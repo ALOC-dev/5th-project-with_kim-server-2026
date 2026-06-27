5b19552385bae6813335f675909da18552390cb4
ktollonr <agetor3210@naver.com>
2026-06-27 20:34:40 +0900

    feat: add global exception handling

--- src/main/java/com/with_kim/aloc_study/service/UserService.java
+++ src/main/java/com/with_kim/aloc_study/service/UserService.java
@@ -3,6 +3,7 @@ package com.with_kim.aloc_study.service;
 import com.with_kim.aloc_study.dto.request.LoginRequest;
 import com.with_kim.aloc_study.dto.response.LoginResponse;
 import com.with_kim.aloc_study.entity.Users;
+import com.with_kim.aloc_study.exception.AuthenticationFailedException;
 import com.with_kim.aloc_study.repository.UserRepository;
 import com.with_kim.aloc_study.security.JwtProvider;
 import jakarta.servlet.http.HttpServletResponse;
@@ -35,7 +36,7 @@ public class UserService {
         String password = request.getPassword();
 
         Users user = userRepository.findByUsername(request.getUsername())
-                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 사용자입니다."));
+                .orElseThrow(() -> new AuthenticationFailedException("존재하지 않는 사용자입니다."));
 
         if(!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
             throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
